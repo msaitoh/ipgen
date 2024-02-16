19a6b48c6648c0e380445f446e240bd1d1ea2e28
Masanobu SAITOH <msaitoh@execsw.org>
2024-02-16 13:25:53 +0900

    Correct indent. No functional change.

--- gen/gen.c
+++ gen/gen.c
@@ -2676,7 +2676,7 @@ rfc2544_showresult(void)
 	 */
 
 
-	 /* check link speed. 1G or 10G? */
+	/* check link speed. 1G or 10G? */
 	tmp = 0 ;
 	for (i = 0; i < rfc2544_ntest; i++) {
 		struct rfc2544_work *work = &rfc2544_work[i];
@@ -2976,8 +2976,8 @@ rfc2544_test(unsigned int n)
 		    work->pktsize,
 		    work->curpps,
 		    calc_mbps(work->pktsize, work->curpps),
-				calc_mbps(work->pktsize, work->minpps),
-				calc_mbps(work->pktsize, work->maxpps));
+		    calc_mbps(work->pktsize, work->minpps),
+		    calc_mbps(work->pktsize, work->maxpps));
 
 		memcpy(&statetime, &currenttime_main, sizeof(struct timeval));
 		statetime.tv_sec += opt_rfc2544_warming_duration;
