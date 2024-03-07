9383cd2d6ab91c728c6ecc347ab6d80bd7ad5dd2
Ryota Ozaki <ozaki-r@iij.ad.jp>
2024-03-07 17:18:17 +0900

    Fix interface_is_active on Linux

--- gen/util.c
+++ gen/util.c
@@ -229,11 +229,10 @@ interface_is_active(const char *ifname)
 	char buf[256];
 #ifdef __linux__
 	active = 0;
-	snprintf(buf, sizeof(buf), "ip link show up dev %s", ifname);
+	snprintf(buf, sizeof(buf), "ip link show %s", ifname);
 	fp = popen(buf, "r");
 	fgets(buf, sizeof(buf), fp);
-	/* If the interface is down, it returns empty string */
-	if (strnlen(buf, sizeof(buf) - 1) != 0)
+	if (strstr(buf, "state UP") != NULL)
 		active = 1;
 	pclose(fp);
 #else
